068bbc6accab6c467cb484a1545c37e35e143e08
Binh Nguyen <binhnguyen280498@gmail.com>
2026-02-07 15:36:55 +0700

    feat: update scroll job

--- src/modules/admin/scroll-job/scroll-job.scheduler.ts
+++ src/modules/admin/scroll-job/scroll-job.scheduler.ts
@@ -3,6 +3,7 @@ import { Cron, CronExpression } from '@nestjs/schedule';
 import { logger } from 'shared/logger/app.logger';
 import { WorkerManager } from 'shared/worker/worker.manager';
 
+import { CRAWL_POST_WORKER_NAME } from '../crawl-post/crawl-post.constant';
 import { CRAWL_PROCESS_WORKER_NAME } from '../crawl-process/crawl-process.constant';
 import { SYNC_TO_WP_WORKER_NAME } from '../sync-to-wp/sync-to-wp.constant';
 import { UPLOAD_STORY_MEDIA_TO_STORAGE_WORKER_NAME } from '../upload-story-media-to-storage/upload-story-media-to-storage.constant';
@@ -12,7 +13,7 @@ import { UPLOAD_THUMBNAIL_POST_TO_STORAGE_WORKER_NAME } from '../upload-thumbnai
 export class ScrollJobScheduler {
   constructor(private readonly workerManager: WorkerManager) {}
 
-  @Cron(CronExpression.EVERY_DAY_AT_4AM)
+  @Cron(CronExpression.EVERY_DAY_AT_6PM)
   async handleCrawlProcessJob(): Promise<void> {
     try {
       // Check if worker is already running
@@ -31,23 +32,23 @@ export class ScrollJobScheduler {
     }
   }
 
-  // @Cron(CronExpression.EVERY_8_HOURS) // Runs at 0:00, 8:00, 16:00
-  // async handleCrawlPostJob(): Promise<void> {
-  //   try {
-  //     if (this.workerManager.isWorkerRunning(CRAWL_POST_WORKER_NAME)) {
-  //       logger.warn('[ScrollJobScheduler] Crawl post worker is already running, skipping...');
+  @Cron(CronExpression.EVERY_8_HOURS)
+  async handleCrawlPostJob(): Promise<void> {
+    try {
+      if (this.workerManager.isWorkerRunning(CRAWL_POST_WORKER_NAME)) {
+        logger.warn('[ScrollJobScheduler] Crawl post worker is already running, skipping...');
 
-  //       return;
-  //     }
+        return;
+      }
 
-  //     logger.info('[ScrollJobScheduler] Starting crawl post job via cron schedule');
-  //     this.workerManager.startJob(CRAWL_POST_WORKER_NAME).catch((error) => {
-  //       logger.error(`[ScrollJobScheduler] Error in crawl post job: ${error}`);
-  //     });
-  //   } catch (error) {
-  //     logger.error(`[ScrollJobScheduler] Error starting crawl post job: ${error}`);
-  //   }
-  // }
+      logger.info('[ScrollJobScheduler] Starting crawl post job via cron schedule');
+      this.workerManager.startJob(CRAWL_POST_WORKER_NAME).catch((error) => {
+        logger.error(`[ScrollJobScheduler] Error in crawl post job: ${error}`);
+      });
+    } catch (error) {
+      logger.error(`[ScrollJobScheduler] Error starting crawl post job: ${error}`);
+    }
+  }
 
   // @Cron('0 4,12,20 * * *') // Runs at 4:00, 12:00, 20:00 (every 8 hours, offset by 4 hours)
   // async handleCrawlLinkMediaJob(): Promise<void> {
