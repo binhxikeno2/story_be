315a0bae7b71bf500bff7554455b733b73747fd6
Binh Nguyen <binhnguyen280498@gmail.com>
2026-02-07 17:09:36 +0700

    feat: update scroll job crawl-process

--- src/modules/admin/scroll-job/scroll-job.scheduler.ts
+++ src/modules/admin/scroll-job/scroll-job.scheduler.ts
@@ -13,7 +13,7 @@ import { UPLOAD_THUMBNAIL_POST_TO_STORAGE_WORKER_NAME } from '../upload-thumbnai
 export class ScrollJobScheduler {
   constructor(private readonly workerManager: WorkerManager) {}
 
-  @Cron('50 18 * * *') // Every day at 18:50
+  @Cron(CronExpression.EVERY_DAY_AT_7PM) // Every day at 18:50
   async handleCrawlProcessJob(): Promise<void> {
     try {
       // Check if worker is already running
