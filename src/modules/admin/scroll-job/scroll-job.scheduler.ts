e9e54919cb03ac058eba0981a4301411cf8e403d
Binh Nguyen <binhnguyen280498@gmail.com>
2026-02-07 19:25:41 +0700

    feat: trigger build

--- src/modules/admin/scroll-job/scroll-job.scheduler.ts
+++ src/modules/admin/scroll-job/scroll-job.scheduler.ts
@@ -14,7 +14,7 @@ export class ScrollJobScheduler {
   constructor(private readonly workerManager: WorkerManager) {}
 
   //trigger
-  @Cron(CronExpression.EVERY_DAY_AT_7PM) // Every day at 18:50
+  @Cron(CronExpression.EVERY_DAY_AT_10PM) // Every day at 18:50
   async handleCrawlProcessJob(): Promise<void> {
     try {
       // Check if worker is already running
