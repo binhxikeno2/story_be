2fe19fefc3aef3334c05c8f07b2890eb2cc1ece6
Binh Nguyen <binhnguyen280498@gmail.com>
2026-04-08 20:36:44 +0700

    feat: trigger build

--- src/modules/admin/scroll-job/scroll-job.scheduler.ts
+++ src/modules/admin/scroll-job/scroll-job.scheduler.ts
@@ -14,7 +14,7 @@ import { UPLOAD_THUMBNAIL_POST_TO_STORAGE_WORKER_NAME } from '../upload-thumbnai
 export class ScrollJobScheduler {
   constructor(private readonly workerManager: WorkerManager) {}
 
-  //trigger render
+  //trigger
   @Cron(CronExpression.EVERY_8_HOURS) // Every day at 18:50
   async handleCrawlProcessJob(): Promise<void> {
     try {
