81dd5e0a90f48703caeb7957b878c93d92fb00de
Binh Nguyen <binhnguyen280498@gmail.com>
2026-05-03 22:48:28 +0700

    feat: update limit story

--- src/database/repositories/story.repository.ts
+++ src/database/repositories/story.repository.ts
@@ -23,20 +23,13 @@ export class StoryRepository extends BaseRepository<StoryEntity> {
   }
 
   async getStoriesWithEmptyInternalUrl(): Promise<Partial<StoryEntity>[]> {
-    return (
-      this.createQueryBuilder('story')
-        .select('story.id', 'id')
-        .addSelect('story.rapid_gator_url', 'rapidGatorUrl')
-        .where('(story.internal_url IS NULL OR story.internal_url = "")')
-        .andWhere('story.rapid_gator_url IS NOT NULL')
-        .andWhere('story.deletedAt IS NULL')
-        //TODO need to improve
-        .andWhere('story.title NOT LIKE :gb', { gb: '%GB%' })
-
-        // .orderBy('story.id', 'ASC')
-        .orderBy(`CAST(REGEXP_SUBSTR(story.title, '[0-9]+(?= MB)') AS UNSIGNED)`, 'ASC')
-        .limit(LIMIT_STORY)
-        .getRawMany()
-    );
+    return this.createQueryBuilder('story')
+      .select('story.id', 'id')
+      .addSelect('story.rapid_gator_url', 'rapidGatorUrl')
+      .where('(story.internal_url IS NULL OR story.internal_url = "")')
+      .andWhere('story.rapid_gator_url IS NOT NULL')
+      .andWhere('story.deletedAt IS NULL')
+      .limit(LIMIT_STORY)
+      .getRawMany();
   }
 }
